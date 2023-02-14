30417ed67cad7c2d3d6c4554895474e883511014
Boris <BacR009@yandex.ru>
2023-02-14 20:44:26 +0500

    Add Bootstrap

--- demo-web/src/main/java/com/example/demoweb/model/Post.java
+++ demo-web/src/main/java/com/example/demoweb/model/Post.java
@@ -1,21 +1,25 @@
 package com.example.demoweb.model;
 
+import java.util.Date;
+
 public class Post {
 
-    public Post(String text)
+    public Post(String text,Date creationDate)
     {
         this.text=text;
         this.likes=0;
+        this.creationDate= creationDate;
     }
 
     private String text;
 
+    private Date creationDate;
     private Integer likes;
 
     public String getText() {
         return this.text;
     }
-
+    public Date getCreationDate() { return  this.creationDate;}
     public Integer getLikes() {
         return likes;
     }
--- demo-web/src/main/java/com/example/demoweb/service/PostService.java
+++ demo-web/src/main/java/com/example/demoweb/service/PostService.java
@@ -3,20 +3,22 @@ package com.example.demoweb.service;
 import com.example.demoweb.model.Post;
 import org.springframework.stereotype.*;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 import java.util.Random;
 
 @Service
 public class PostService {
     public List<Post> listAllPosts(){
-        Post p1=new Post("Купил мужик шляпу");
-        Post p2 =new Post("А она ему...");
+        Post p1=new Post("Купил мужик шляпу", new Date(2023, 02,14));
+        Post p2 =new Post("А она ему...", new Date(2023, 02,14));
         Random random=new Random();
         String result = random.nextInt()%2==0
                 ? "Как раз"
                 : "Не подошла";
-        Post p3= new Post(result);
+        Post p3= new Post(result, new Date(2023, 02,14));
         return Arrays.asList(p1,p2,p3);
     }
 
